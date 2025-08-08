a07c17cc7e022f1a67c01943364fc48ae81371aa
ohtaisao <tigerwoods1206@gmail.com>
2025-08-08 11:17:41 +0900

    update

--- Assets/NorthStar/Interaction/PhysicalHand.cs
+++ Assets/NorthStar/Interaction/PhysicalHand.cs
@@ -1,13 +1,15 @@
 // Copyright (c) Meta Platforms, Inc. and affiliates.
 using Oculus.Interaction;
-using Oculus.Interaction.HandGrab;
 using Oculus.Interaction.Input;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.Events;
+
 namespace NorthStar
 {
     /// <summary>
     /// Script that handles moving the hand objects as physical objects
+    /// XR Interaction Toolkit 連携のため HandGrabInteractor 依存を排除
     /// </summary>
     [RequireComponent(typeof(Rigidbody))]
     public class PhysicalHand : MonoBehaviour
@@ -17,7 +19,7 @@ namespace NorthStar
         [SerializeField] private Rigidbody m_parent;
         private Vector3 m_lastBodyPosition;
         [SerializeField, Interface(typeof(IHand))] private Object m_handObject;
-       
+
         public HandColliders Colliders { get; private set; }
 
         public float LocalMovementStrengthModifier = 1;
@@ -31,7 +33,11 @@ namespace NorthStar
         [SerializeField] private Vector3 m_jointAnchor;
         public Vector3 JointAnchorRotation => m_jointAnchorRotation;
         [SerializeField] private Vector3 m_jointAnchorRotation;
-        [SerializeField] private HandGrabInteractor m_interactor;
+
+        // XRIT 側で利用できる「現在つかみ可能か」のフラグ（以前の HandGrabInteractor.enabled 相当）
+        public bool IsInteractionAllowed { get; private set; }
+        [SerializeField] private UnityEvent<bool> m_onInteractionAllowedChanged;
+
         public Rigidbody Rigidbody { get; private set; }
         public Rigidbody WristBody { get; private set; }
         private float m_breakTimer;
@@ -40,7 +46,7 @@ namespace NorthStar
         private void Awake()
         {
             m_joint = GetComponent<ConfigurableJoint>();
-            m_rotationJoint = GetComponent<ConfigurableJoint>();// = transform.GetChild(0).GetComponent<ConfigurableJoint>();
+            m_rotationJoint = GetComponent<ConfigurableJoint>(); // 必要なら別のジョイントに差し替え
             Colliders = GetComponent<HandColliders>();
             Rigidbody = GetComponent<Rigidbody>();
             WristBody = m_rotationJoint.GetComponent<Rigidbody>();
@@ -48,6 +54,7 @@ namespace NorthStar
 
         private bool GetHandEnabled()
         {
+            // XRIT の手検出に置き換える場合はここで状態を返す
             return true;
         }
 
@@ -56,11 +63,15 @@ namespace NorthStar
             Rigidbody.position = m_handAnchor.position;
             Rigidbody.rotation = m_handAnchor.rotation;
             Rigidbody.isKinematic = false;
+            m_connected = true;
+            UpdateInteractionAllowed();
         }
 
         private void OnHandDisconnected()
         {
             Rigidbody.isKinematic = true;
+            m_connected = false;
+            UpdateInteractionAllowed();
         }
 
         public void StartInteraction()
@@ -82,7 +93,6 @@ namespace NorthStar
         private void OnValidate()
         {
             m_joint = GetComponent<ConfigurableJoint>();
-
             m_joint.connectedBody = m_boundToParent ? m_parent : null;
         }
 
@@ -92,29 +102,23 @@ namespace NorthStar
             DampendSpringJoint.TargetRotation = m_handAnchor.rotation;
 
             m_rotationJoint.targetRotation = m_handAnchor.rotation;
-            if (GlobalSettings.PlayerSettings.MaxHandDistance != float.PositiveInfinity && Vector3.Distance(transform.position, m_handAnchor.position) > GlobalSettings.PlayerSettings.MaxHandDistance)
+
+            if (GlobalSettings.PlayerSettings.MaxHandDistance != float.PositiveInfinity &&
+                Vector3.Distance(transform.position, m_handAnchor.position) > GlobalSettings.PlayerSettings.MaxHandDistance)
             {
                 Rigidbody.position = m_handAnchor.position;
                 transform.position = Rigidbody.position;
             }
+
             DampendSpringJoint.AddForce();
         }
+
         private void Update()
         {
-            /*
-            if ((!GlobalSettings.PlayerSettings.HandsReleaseOnInvalidPosition) || BodyPositions.Instance.IsHandWithinLimits(m_bone))
-            {
-                m_breakTimer -= Time.deltaTime;
-            }
-            else
-            {
-                m_breakTimer += Time.deltaTime;
-            }
-            */
-            
+            // 解除タイマー（XRIT 側で参照して強制解除などに利用可能）
             m_breakTimer += Time.deltaTime;
             m_breakTimer = Mathf.Clamp(m_breakTimer, 0.0f, GlobalSettings.PlayerSettings.HandBreakTimeout + ExcessBreakTimer);
-            m_interactor.enabled = m_breakTimer < GlobalSettings.PlayerSettings.HandBreakTimeout + ExcessBreakTimer && m_connected;
+
             if (!GetHandEnabled())
             {
                 Rigidbody.position = m_handAnchor.position;
@@ -123,15 +127,30 @@ namespace NorthStar
                     OnHandDisconnected();
                 return;
             }
+
             if (!m_connected)
                 OnHandConnected();
+
+            UpdateInteractionAllowed();
+        }
+
+        private void UpdateInteractionAllowed()
+        {
+            var allowed = m_breakTimer < GlobalSettings.PlayerSettings.HandBreakTimeout + ExcessBreakTimer && m_connected;
+            if (allowed != IsInteractionAllowed)
+            {
+                IsInteractionAllowed = allowed;
+                m_onInteractionAllowedChanged?.Invoke(allowed); // XRIT ブリッジ側で受け取り可
+            }
         }
 
         public void ForceDropHeldItem()
         {
             if (!GlobalSettings.PlayerSettings.AllowForcedHandBreak)
                 return;
+
             m_breakTimer = GlobalSettings.PlayerSettings.HandBreakTimeout + ExcessBreakTimer;
+            UpdateInteractionAllowed();
         }
 
         private void FixedUpdate()
